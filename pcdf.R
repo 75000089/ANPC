c08d62fbab77c63c9fd395e02167d12384f5348d
75000089 <118511380+75000089@users.noreply.github.com>
2023-05-22 16:29:33 +0800

    Delete pcdf.R

--- pcdf.R
+++
@@ -1,6 +0,0 @@
-#make the dataframe of the principal component scores and metadata. This will be used in the plots. The pca
-
-pcdf<- function(results, annotation){
-  pcdf<-cbind(as.data.frame(results[["x"]]), annotation)
-  return(pcdf)
-}
