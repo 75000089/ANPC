0ee9b3ed857bed04e249f3159c628e26a8c2761a
Lucy <lucy@example.com>
2023-05-24 11:54:01 +0800

    fully functional pcagrid with working legend and selections

--- R/pcagrid.R
+++ R/pcagrid.R
@@ -2,7 +2,7 @@
 
 pcagrid <-function(screecumulativethresholdobject, CO, SH = NULL, SZ = 0.2, AL = 1, COtitle, SHtitle = "NULL", SZtitle = "NULL", ALtitle = "NULL"){
 
-  output <- plotinput(screecumulativethresholdobject, CO, SH, SZ, AL)
+  output <- plotinput(screecumulativethresholdobject = screecumulativethresholdobject, CO = CO, SH = SH, SZ = SZ, AL = AL)
   thresh <- output$data$treshold
   test <- pcagridlegend(screecumulativethresholdobject, x = "PC1", y = "PC2", CO, SH, AL, SZ, COtitle, SHtitle, SZtitle, ALtitle)
   title <- list()
@@ -10,6 +10,24 @@ pcagrid <-function(screecumulativethresholdobject, CO, SH = NULL, SZ = 0.2, AL =
     title[[i]] <- paste0('PC', i, ' (', round(output$data$explained_variance_ratio[i], 1), '%)')
   }
   title<-unlist(title)
+  #gp <- geom_point(aes(colour = output$CO, size = output$SZ), shape = output$SH, alpha = output$AL)
+  gp <- if(class(SH) == "character" & class(SZ) == "numeric" & class(AL) == "numeric") {
+          geom_point(aes(colour = output$CO, shape = output$SH), size = output$SZ, alpha = output$AL)
+        } else if (class(SH) == "NULL" & class(SZ) == "character" & class(AL) == "numeric") {
+          geom_point(aes(colour = output$CO, size = output$SZ), shape = output$SH, alpha = output$AL)
+        } else if (class(SH) == "NULL" & class(SZ) == "numeric" & class(AL) == "character") {
+          geom_point(aes(colour = output$CO, alpha = output$AL), shape = output$SH, size = output$SZ)
+        } else if (class(SH) == "character" & class(SZ) == "character" & class(AL) == "numeric") {
+          geom_point(aes(colour = output$CO, shape = output$SH, size = output$SZ), alpha = output$AL)
+        } else if (class(SH) == "character" & class(SZ) == "numeric" & class(AL) == "character") {
+          geom_point(aes(colour = output$CO, shape = output$SH, alpha = output$AL), size = output$SZ)
+        } else if (class(SH) == "NULL" & class(SZ) == "character" & class(AL) == "character") {
+          geom_point(aes(colour = output$CO, size = output$SZ, alpha = output$AL), shape = output$SH)
+        } else if (class(SH) == "character" & class(SZ) == "character" & class(AL) == "character") {
+          geom_point(aes(colour = output$CO, shape = output$SH, size = output$SZ, alpha = output$AL))
+        } else if (class(SH) == "NULL" & class(SZ) == "numeric" & class(AL) == "numeric") {
+          geom_point(aes(colour = output$CO), shape = output$SH, size = output$SZ, alpha = output$AL)
+        }
 
   pcagridplot<-GGally::ggpairs(data = output$data$pcdf[,1:thresh],
                                columnLabels = c(title),
@@ -20,7 +38,7 @@ pcagrid <-function(screecumulativethresholdobject, CO, SH = NULL, SZ = 0.2, AL =
                                legend = grab_legend(test),
                                progress = F,
                                switch = "both") +
-                               geom_point(aes(colour = output$CO, shape = output$SH, size = output$SZ, alpha = output$AL)) +
+                               gp +
                                stat_ellipse(aes(group=interaction(output$CO, color=output$CO), color=output$CO))+
                                theme_bw() +
                                theme(strip.background = element_rect(fill = "white"),
--- R/plotinput.R
+++ R/plotinput.R
@@ -1,23 +1,26 @@
 #This function handles the shape (SH), size (SZ) and alpha (AL) input for the pcagrid.
-
 plotinput<- function(screecumulativethresholdobject, CO, SH, SZ, AL){
 
   output <- screecumulativethresholdobject
 
-  # Get required data for plotting
   #colour
   CO <- if(class(CO) == "numeric") {
-    CO = CO
+    output$data$pcdf$CO = rep_len(CO, nrow(output$data$pcdf))
+    CO = output$data$pcdf$CO
   }else if(class(CO) == "character"){
     CO = output$data$pcdf[,CO]
   }
 
-  #size
-  SZ <- if(class(SZ) == "numeric") {
-    SZ = SZ
-  }else if (class(SZ) == "character"){
-    SZ = output$data$pcdf[,SZ]
-  }
+  # if(class(CO) == "numeric"){
+  #   output$data$pcdf$CO = rep_len(CO, nrow(output$data$pcdf))
+  # }
+  # CO = output$data$pcdf[,CO]
+
+    SZ <- if(class(SZ) == "numeric") {
+      SZ = SZ
+    }else if (class(SZ) == "character"){
+      SZ = output$data$pcdf[,SZ]
+    }
 
   #alpha
   AL <- if(class(AL) == "numeric") {
@@ -28,15 +31,65 @@ plotinput<- function(screecumulativethresholdobject, CO, SH, SZ, AL){
 
   #shape
   SH <- if(class(SH) == "NULL") {
-    SH = SH
+     output$data$pcdf$SH = rep_len("circle", nrow(output$data$pcdf))
+     SH = output$data$pcdf$SH
   }else if(class(SH) == "character"){
     SH = output$data$pcdf[,SH]
   }
 
   output<- append(output, list(CO=CO,
-                              SH = SH,
-                              SZ = SZ,
-                              AL = AL))
+                               SH = SH,
+                               SZ = SZ,
+                               AL = AL))
 
-return(output)
+  return(output)
 }
+# plotinput<- function(screecumulativethresholdobject, CO, SH, SZ, AL){
+#
+#   output <- screecumulativethresholdobject
+#
+#   # Get required data for plotting
+#   #colour
+#   CO <- if(class(CO) == "numeric") {
+#     output$data$pcdf$CO = rep_len(CO, nrow(output$data$pcdf))
+#     CO = output$data$pcdf[,CO]
+#   }else if(class(CO) == "character"){
+#     CO = output$data$pcdf[,CO]
+#   }
+#
+#   # if(class(CO) == "numeric"){
+#   #   output$data$pcdf$CO = rep_len(CO, nrow(output$data$pcdf))
+#   # }
+#   # CO = output$data$pcdf[,CO]
+#
+#   #size
+#   SZ <- if(class(SZ) == "numeric") {
+#     output$data$pcdf$SZ = rep_len(SZ, nrow(output$data$pcdf))
+#     SZ = output$data$pcdf[,SZ]
+#   }else if (class(SZ) == "character"){
+#     SZ = output$data$pcdf[,SZ]
+#   }
+#
+#   #alpha
+#   AL <- if(class(AL) == "numeric") {
+#     output$data$pcdf$AL = rep_len(AL, nrow(output$data$pcdf))
+#     AL = output$data$pcdf[,AL]
+#   }else if (class(AL) == "character"){
+#     AL = output$data$pcdf[,AL]
+#   }
+#
+#   #shape
+#   SH <- if(class(SH) == "NULL") {
+#     #output$data$pcdf$SH = rep_len(1, nrow(output$data$pcdf))
+#     SH = 1
+#   }else if(class(SH) == "character"){
+#     SH = output$data$pcdf[,SH]
+#   }
+#
+#   output<- append(output, list(CO=CO,
+#                               SH = SH,
+#                               SZ = SZ,
+#                               AL = AL))
+#
+# return(output)
+# }
