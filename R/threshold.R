31bd222630ffd01523479a07d36cfe97fa56ce5f
75000089 <118511380+75000089@users.noreply.github.com>
2023-05-29 15:06:53 +0800

    Delete threshold.R

--- R/threshold.R
+++
@@ -1,8 +0,0 @@
-#Calculate a threshold for the number of principal components that explain, as a default, 99% of the variance.
-threshold<-function(data, cutoff = 99){
-
- a<-pcresults(data)
-
-  t <-length(which(a$pcsum$`Cumulative Proportion` < cutoff))
-  return(append(a, list(threshold=t)))
-}
